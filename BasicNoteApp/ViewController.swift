e4a3cfa6bcdecc88b987462d0aed4cf520fb0d19
Ali ahmet Erdoğdu <aliahmeterdogdu@192.168.1.105>
2024-07-01 14:33:47 +0300

    initial commit

---
+++ BasicNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicNoteApp
+//
+//  Created by Ali ahmet Erdoğdu on 1.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
