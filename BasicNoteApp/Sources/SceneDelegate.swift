8e2e633d45b673effa2b4c0eb31147b23fc42229
ALİ AHMET ERDOĞDU <082aliahmet4@gmail.com>
2024-07-21 18:49:53 +0300

    Initial Page Setting

--- BasicNoteApp/Sources/SceneDelegate.swift
+++ BasicNoteApp/Sources/SceneDelegate.swift
@@ -16,7 +16,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if let token = UserDefaults.standard.value(forKey: "accesToken") {
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            let window = UIWindow(windowScene: windowScene)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+            // İlk açılacak ViewController'ın storyboardID'sini belirtin
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "NotesScreenViewController")
+            let navigationController = UINavigationController(rootViewController: initialViewController)
+            
+            window.rootViewController = navigationController
+            self.window = window
+            window.makeKeyAndVisible()
+
+        } else {
+            guard let _ = (scene as? UIWindowScene) else { return }
+        }
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
